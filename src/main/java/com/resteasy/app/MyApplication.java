a13d08abea63f1bf8781b02ef6beeed0989e2550
Parvej Muhamad <pmu.axelor@gmail.com>
2021-09-14 18:21:43 +0530

    example using Get method

---
+++ src/main/java/com/resteasy/app/MyApplication.java
@@ -0,0 +1,8 @@
+package com.resteasy.app;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+   
+}
